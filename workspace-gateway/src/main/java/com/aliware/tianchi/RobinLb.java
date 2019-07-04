b3e72b53144a6ed860062ed4512ca80fb8078016
renjh2011 <923526125@qq.com>
2019-07-04 13:43:25 +0800

    增加服务器端获取线程池状态，客户端处理线程池状态。

---
+++ workspace-gateway/src/main/java/com/aliware/tianchi/RobinLb.java
@@ -0,0 +1,99 @@
+package com.aliware.tianchi;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class RobinLb {
+    private AtomicInteger weight = new AtomicInteger(0);
+    private AtomicInteger curWeight = new AtomicInteger(0);
+    private AtomicInteger lastWeight = new AtomicInteger(0);
+    private AtomicInteger port = new AtomicInteger(0);
+
+    public RobinLb(Integer weight, Integer curWeight, Integer port) {
+        this.weight.set(weight);
+        this.lastWeight.set(weight);
+        this.curWeight.set(curWeight);
+        this.port.set(port);
+    }
+
+    private final static ConcurrentMap<Integer,RobinLb> SERVER_MAP =  new ConcurrentHashMap<>();
+    static {
+        SERVER_MAP.putIfAbsent(20880,new RobinLb(200,0,20880));
+        SERVER_MAP.putIfAbsent(20870,new RobinLb(450,0,20870));
+        SERVER_MAP.putIfAbsent(20890,new RobinLb(600,0,20890));
+    }
+
+    public static void main(String[] args) {
+        for(int i=0;i<25;i++) {
+            getServer();
+        }
+    }
+    public static void getServer(){
+        ConcurrentMap<Integer,Integer> tempWeight = new ConcurrentHashMap<>(SERVER_MAP.size());
+        Iterator<Map.Entry<Integer,RobinLb>> iterator = SERVER_MAP.entrySet().iterator();
+        int maxWeight=0;
+        int totalWight = 0;
+        RobinLb maxWeightRobinLb = null;
+        while (iterator.hasNext()){
+            Map.Entry<Integer,RobinLb> entry = iterator.next();
+            int realWeight = entry.getValue().lastWeight.get();
+            if(maxWeight<realWeight){
+                maxWeight=entry.getValue().lastWeight.get();
+                maxWeightRobinLb=entry.getValue();
+            }
+            totalWight+=entry.getValue().getWeight().get();
+        }
+
+        iterator = SERVER_MAP.entrySet().iterator();
+        while (iterator.hasNext()){
+            Map.Entry<Integer,RobinLb> entry = iterator.next();
+            RobinLb robinLb = entry.getValue();
+            if(maxWeightRobinLb.getPort().get()==robinLb.getPort().get()){
+                int curTempWeight = robinLb.getWeight().get()+(robinLb.lastWeight.get()-totalWight);
+                robinLb.setCurWeight(curTempWeight);
+                robinLb.lastWeight.set(curTempWeight);
+            }else {
+                int curTempWeight = robinLb.lastWeight.get();
+                robinLb.setCurWeight(robinLb.getWeight().get()+curTempWeight);
+                robinLb.lastWeight.set(robinLb.getWeight().get()+curTempWeight);
+            }
+
+        }
+
+        System.out.println(maxWeightRobinLb);
+
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight.set(weight);
+    }
+
+    public void setCurWeight(Integer curWeight) {
+        this.curWeight.set(curWeight);
+    }
+
+    public AtomicInteger getWeight() {
+        return weight;
+    }
+
+    public AtomicInteger getCurWeight() {
+        return curWeight;
+    }
+
+    public AtomicInteger getPort() {
+        return port;
+    }
+
+    @Override
+    public String toString() {
+        return "RobinLb{" +
+                "weight=" + weight +
+                ", curWeight=" + curWeight +
+                ", port=" + port +
+                ", lastWeight=" + lastWeight +
+                '}';
+    }
+}
