19d5a8a21c6eb89938b3dacb46f8fce64957cd53
renjh2011 <923526125@qq.com>
2019-07-05 23:31:31 +0800

    commit undo queue

--- workspace-provider/src/main/java/com/aliware/tianchi/TestServerFilter.java
+++ workspace-provider/src/main/java/com/aliware/tianchi/TestServerFilter.java
@@ -2,16 +2,10 @@ package com.aliware.tianchi;
 
 import org.apache.dubbo.common.Constants;
 import org.apache.dubbo.common.extension.Activate;
-import org.apache.dubbo.rpc.Filter;
-import org.apache.dubbo.rpc.Invocation;
-import org.apache.dubbo.rpc.Invoker;
-import org.apache.dubbo.rpc.Result;
-import org.apache.dubbo.rpc.RpcException;
+import org.apache.dubbo.rpc.*;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author daofeng.xjf
@@ -22,18 +16,11 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 @Activate(group = Constants.PROVIDER)
 public class TestServerFilter implements Filter {
-    private final static ArrayBlockingQueue<InvokerInvacation> buffer = new ArrayBlockingQueue<>(ServerStatus.getMaxThreadPool()+100);
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         try{
-            InvokerInvacation invokerInvacation = new InvokerInvacation(invoker,invocation);
-            buffer.offer(invokerInvacation);
-            buffer.take();
             Result result = invoker.invoke(invocation);
             return result;
-        }
-        catch (InterruptedException e){
-            throw new RpcException(e.getMessage());
         }catch (Exception e){
             throw e;
         }
