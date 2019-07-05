c2dcdeb0022d626f1feb280987a19be741f81b39
renjh2011 <923526125@qq.com>
2019-07-06 00:46:22 +0800

    commit sub fail time

--- workspace-gateway/src/main/java/com/aliware/tianchi/RobinLb.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/RobinLb.java
@@ -41,13 +41,13 @@ public class RobinLb {
             Map.Entry<Integer,RobinLb> entry = iterator.next();
             RobinLb robinLb = entry.getValue();
             if(entry.getKey().equals(port)){
-                robinLb.setWeight(robinLb.getWeight().get()-5);
+                robinLb.setWeight(robinLb.getWeight().get()-1);
                 robinLb.setCurWeight(0);
                 continue;
             }
             ClientStatus clientStatus = ClientStatus.getStatus(ip,port);
-            if(System.currentTimeMillis()-clientStatus.failedTime.get()>1000) {
-                robinLb.setWeight(robinLb.getWeight().get() + 10);
+            if(System.currentTimeMillis()-clientStatus.failedTime.get()>500) {
+                robinLb.setWeight(robinLb.getWeight().get() + 15);
                 entry.getValue().setCurWeight(0);
             }
         }
